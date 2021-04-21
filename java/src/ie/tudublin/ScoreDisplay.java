01d67a42c64ce1844e125fdb84946412e88a243c
hussain992077 <hussaini992077@gmail.com>
2021-04-21 20:29:10 +0100

    SCORE

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -4,12 +4,39 @@ import java.util.ArrayList;
 
 
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class ScoreDisplay extends PApplet
 {
+	
+	ArrayList<Note> notes = new ArrayList<Note>();
+
+
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	
+	public void loadScore()
+	{
+		Table t = loadTable("note.csv","header");
+		for(TableRow r:t.rows())
+		{
+			Note note = new Note(r);
+			notes.add(note);
+		}
+		
+	}
+	
+
+	public void printScores()
+	{
+		for(Note t:notes)
+		{
+			println(t);
+		}		
+	}
+	
 	
 	public void settings()
 	{
@@ -23,7 +50,8 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		
+		loadScore();
+		printScores();
 	}
 
 	public void draw()
@@ -32,7 +60,8 @@ public class ScoreDisplay extends PApplet
 		
 	}
 
-	void drawNotes()
-	{
-	}
+
+	//void drawNotes()
+	//{
+	//}
 }
